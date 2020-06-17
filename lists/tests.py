01cde214843d53bd9935fa6d64aa1bd1073d23f6
daylanvig <daylanvig@gmail.com>
2020-06-17 07:34:47 -0600

    Add separate list page

--- lists/tests.py
+++ lists/tests.py
@@ -22,20 +22,26 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data = {'item_text': 'A new list item'})
         # post redirect get
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_save_item_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+        
     def test_displays_all_list_items(self):
         Item.objects.create(text='first item')
         Item.objects.create(text='second item')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         
-        self.assertIn('first item', response.content.decode())
-        self.assertIn('second item', response.content.decode())
+        self.assertContains(response, 'first item')
+        self.assertContains(response, 'second item')
 
 class ItemModelTest(TestCase):
 
