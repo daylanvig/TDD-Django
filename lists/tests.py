01f5c93ea7fadf67bb00e56ac47a26b5f051aec3
daylanvig <daylanvig@gmail.com>
2020-06-17 07:53:31 -0600

    Add list table

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -29,24 +29,33 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='first item')
-        Item.objects.create(text='second item')
+        list_ = List.objects.create()
+        Item.objects.create(text='first item', list=list_)
+        Item.objects.create(text='second item', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
         
         self.assertContains(response, 'first item')
         self.assertContains(response, 'second item')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'First item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Second item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -55,4 +64,6 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         
         self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Second item')
+        self.assertEqual(second_saved_item.list, list_)
